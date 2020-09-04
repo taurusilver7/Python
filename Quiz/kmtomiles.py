d5e854f4716605f63d244638a28da102a3158a1d
Devi prasad <61942957+taurusilver7@users.noreply.github.com>
2020-09-04 10:36:07 +0530

    length converter

--- Quiz/kmtomiles.py
+++ Quiz/kmtomiles.py
@@ -8,4 +8,3 @@ while endGame != "quit":
     rqst = requests.get(url)
     if (rqst.status_code != 200):
         endGame = input("Sorry! There was a problem retrieving the question. Press enter to try again or type 'quit' to end the game.")
-        
